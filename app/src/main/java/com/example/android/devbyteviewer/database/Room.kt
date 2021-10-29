5d64bc506b02ced37e0c0dd6c87662dca3a7f860
Igor de Souza <igor.carlosouza@gmail.com>
2021-10-29 11:36:03 -0300

    Create VideosDatabase

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,41 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+
+    abstract val videoDao: VideoDao
+
+    companion object {
+        @Volatile
+        private lateinit var INSTANCE: VideosDatabase
+
+        fun getDatabase (context: Context): VideosDatabase {
+            synchronized(this) {
+                if (!::INSTANCE.isInitialized) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                                                    VideosDatabase::class.java,
+                                                    "videos").build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+
+}
