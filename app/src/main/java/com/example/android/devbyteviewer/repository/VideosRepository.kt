1289f98991e50a150c70dc91dd9b4406ba930fa5
Igor de Souza <igor.carlosouza@gmail.com>
2021-10-30 09:25:27 -0300

    Create repository

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,33 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * Repository for fetching videos from network and storing them on disk.
+ */
+
+class VideosRepository (private val database: VideosDatabase) {
+
+    /**
+     * A playlist of videos that can be shown on screen.
+     */
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) { it.asDomainModel() }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+
+}
